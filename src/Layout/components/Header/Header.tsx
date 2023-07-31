6e56a5427d279179360317b317b58b94ce1f1667
mobacsell <mobacsell@yandex.ru>
2023-07-31 15:39:23 +0300

    feat: add Link comps for logo, Home and order

--- src/Layout/components/Header/Header.tsx
+++ src/Layout/components/Header/Header.tsx
@@ -1,13 +1,29 @@
+import { Link } from "react-router-dom";
 import styles from "./Header.module.scss";
 
 export function Header() {
   return (
     <header className={styles.root}>
-      <div className={styles.logo}>Greeny</div>
+      <div className={styles.logo}>
+        <Link to="./" className={styles.logoLink}>Greeny</Link>
+      </div>
       <nav>
         <ul className={styles.list}>
-          <li className={styles.item}><a href="./" className={styles.link}>Home</a></li>
-          <li className={styles.item}><a href="./" className={styles.link}>Order</a></li>
+          <li className={styles.item}>
+            <Link to="./" className={styles.link}>
+              {" "}
+              Home
+            </Link>
+          </li>
+          <li className={styles.item}>
+            <Link
+              to="./order"
+              className={styles.link}
+              title="Переход на несуществующий заказ"
+            >
+              Order
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
